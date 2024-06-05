c185698c2ecae28aff201cbf05ee0bedc1bd6e10
Chiranjeev Patel <chiranjeev.2125cs@kiet.edu>
2024-06-05 15:50:59 +0530

    Co-authored-by: ASHMITA <ashmitaa199@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,87 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
-import { Link } from "react-router-dom";
 import "./navbar.css"
-import App from "../App";
+import { CgMenuMotion } from "react-icons/cg";
+import { useState } from "react";
+import { useEffect } from "react";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
+
 function Navbar(){
+  const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      if (prevScrollPos > currentScrollPos) {
+        document.getElementById("navbar").classList.remove("hidden");
+      } else {
+        document.getElementById("navbar").classList.add("hidden");
+      }
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos])
 return (
     <>
-    <div className="nav-container">
-        <div className="logo">
-            <h1>CleverBooks</h1>
-        </div>
-        <div className="nav-item">
-            <ul>
-            <li>Product</li>
-                <li>Pricing</li>
-                <li>Industry</li>
-                <li>Coustomer Stories</li>
-                <li>About</li>
-                <li>Blog</li>
-            </ul>
-        </div>
-        <div className="btns">
-        <button>Login</button>
-        <button type="button" class="btn btn-danger">Talk to Us</button>
-        </div>
+   <nav id="navbar" class=" bg-[#151218] flex justify-between w-full items-center py-2 px-4 fixed transition duration-700 ease-in-out z-30 bg-opacity-70">
+    <a href="#">
+        <h1 class="text-3xl text-slate-300">Clever Books</h1>
+    </a>
+    <div class=" hidden md:flex gap-6  ">
+        <a href="" class="text-slate-300 hover:text-red-300 transition duration-150">Product</a>
+        <a href="" class="text-slate-300 hover:text-red-300 transition duration-150">Pricing</a>
+        <a href="" class="text-slate-300 hover:text-red-300 transition duration-150">Industry</a>
+        <a href="" class="text-slate-300 hover:text-red-300 transition duration-150">Coustomer Stories</a>
+        <a href="" class="text-slate-300 hover:text-red-300 transition duration-150">About</a>
+        <a href="" class="text-slate-300 hover:text-red-300 transition duration-150">Blog</a>
     </div>
+    <div class=" hidden md:flex justify-between items-center gap-[1.5rem]">
+        <button className="text-rose-500 hover:text-white transition duration-150">Login</button>
+        <button type="button" class="
+        transition duration-150   text-white bg-[#FFFFFF] font-medium rounded-full text-sm text-center  px-4 py-2 dark:hover:bg-blue-700 dark:focus:ring-blue-800 bg-gradient-to-r from-[salmon] to-[purple] "><span className="hover:text-black">Talk to Us</span></button>
+    </div>
+    <div className="flex md:hidden">
+
+    <Sheet >
+  <SheetTrigger><div className="flex md:hidden text-white mr-10 h-full text-3xl hover:bg-[#11111]">
+    <CgMenuMotion  />
+    </div>
+    </SheetTrigger>
+  <SheetContent >
+    <div className="flex flex-col mt-10 gap-3">
+        <a href="" className="text-[#151218] text-sm font-medium hover:text-red-300 transition duration-150">Product</a>
+        <a href="" class="text-[#151218]  text-sm font-medium hover:text-red-300 transition duration-150">Pricing</a>
+        <a href="" class="text-[#151218]  text-sm font-medium hover:text-red-300 transition duration-150">Industry</a>
+        <a href="" class="text-[#151218]  text-sm font-medium hover:text-red-300 transition duration-150">Coustomer Stories</a>
+        <a href="" class="text-[#151218]  text-sm font-medium hover:text-red-300 transition duration-150">About</a>
+        <a href="" class="text-[#151218] text-sm font-medium hover:text-red-300 transition duration-150">Blog</a>
+        <button className="text-rose-500 hover:text-black transition duration-150">Login</button>
+        <button type="button" class="
+        transition duration-150   text-white bg-[#FFFFFF] font-medium rounded-full text-sm text-center  px-4 py-2 dark:hover:bg-blue-700 dark:focus:ring-blue-800 bg-gradient-to-r from-[salmon] to-[purple] "><span className="hover:text-black">Talk to Us</span></button>
+        </div>
+  </SheetContent>
+</Sheet>
+
+    </div>
+    
+    
+
+    
+   </nav>
+   
     </>
 )
 }
